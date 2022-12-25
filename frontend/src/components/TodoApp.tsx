bd4f323e503d75e00ff2f69d06006317446751d2
licetullmann <licetrosales@gmail.com>
2022-12-25 01:01:24 +0100

    Add addTodo&todo POST req & use spread-op (addTodo) to upd todoList state

--- frontend/src/components/TodoApp.tsx
+++ frontend/src/components/TodoApp.tsx
@@ -1,9 +1,10 @@
 //export default = Standardexport
-import TodoItem from "./TodoItem";
+
 import TodoList from "./TodoList";
 import axios from "axios";
 import {Todo} from "../models/Todo";
 import {useEffect, useState} from "react";
+import AddTodo from "./AddTodo";
 
 export default function TodoApp() {
     const todoBaseUrl = "/api/todo";
@@ -17,12 +18,12 @@ export default function TodoApp() {
 
 // Eine Funktion um "Seiteneffekte" zu verarbeiten
     // Wir nutzen useEffect um eine Endlosschleife zu vermeiden
-    useEffect(()=> {
+    useEffect(() => {
         getTodos()
         //[] : useEffect wird beim ERSTEN laden der Komponente
     }, [])
 
-    function getTodos(){
+    function getTodos() {
         // get wiel HTTP GET
         // Eine Anfrage ist asynchron
         // Um auf die Antwort zu antworten SOBALD sie da ist,
@@ -30,20 +31,61 @@ export default function TodoApp() {
         axios.get(todoBaseUrl)
             // Was passiert, wenn die Anfrage erfolgreich war
 
-            .then(todoListResponse =>{
+            .then(todoListResponse => {
                 const newTodoList: Todo[] = todoListResponse.data;
-            setTodos(newTodoList);
+                setTodos(newTodoList);
             })
-        // ... und wenn's schief läuft ...?
+            // ... und wenn's schief läuft ...?
             .catch(errorMessageResponse => {
                 console.log("ALAM! Es gab einen Fehler beim GET: " + errorMessageResponse)
             })
 
     }
+
+    function addTodo(newTodoWithoutId: Todo) {
+
+        axios.post(todoBaseUrl, newTodoWithoutId)
+            // Statuscode = Erfolgreich 200 Ok
+            // Auf Antwort reagieren
+            .then(newTodoResponse => {
+                console.log("Early Christmas! Neues TODO: " + newTodoResponse.data)
+
+                // Aufgabe 3. Das neue Todo mit der ID zur Liste hinzufügen
+// Wir aktualizieren den "todosList" State und nutzen dafür den vorherigen State mittels
+                // "prev" (= previous) + <stateName>
+                // prevTodoList = vorherige Todo-Liste
+                setTodos(prevTodoList => {
+                        // Hier nutzen wir den "Spread Operator" = ...
+                        // https://stackoverflow.com/a/37002941
+
+                        // Was pasiert: mit den crei Punkten kopieren wir alles aus dem "prevTodos" Array
+                        // und fügen das neue Element zur Liste hinzu
+                        // mit return geben wir die neu erstellte Liste (alte Liste + neues Element) zurück
+                    // Was bei den Pünktchen unter der Haube passiert:
+                    // const newTodoList: Todo[] = []
+                    // for (const todo of prevTodoList) {
+                    // new TodoList.push(todo)
+                    //}
+                    return [...prevTodoList, newTodoResponse.data]
+                    }
+                )
+            })
+            // Fehlercode
+            .catch(errorMessageResponse => {
+
+                console.log("ALARM! Es gab einen Fehler beim POST: " + errorMessageResponse)
+            })
+    }
+
+    //TODO Hinzufügen:
+    // 1. Das neue Todo übergeben
+    // 2. Das neue Todo ans Backend verschicken
+    // 3. Das neue Todo MIT der ID in der Liste speichern
     return (
         <section>
             <h1> Beste Todo App der Welt </h1>
             <TodoList todosToMap={todos}/>
+            <AddTodo handleAddTodo={addTodo}/>
         </section>
     )
 }
